a35044d97a2524229d0417f829febfde0b306371
HENRIQUE <HENRIQUE@DESKTOP-246GAH0>
2023-02-09 14:20:12 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD2/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtCNPJ = New System.Windows.Forms.TextBox()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.txtTelefone = New System.Windows.Forms.TextBox()
+        Me.btnNovo = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnExcluir = New System.Windows.Forms.Button()
+        Me.btnExibir = New System.Windows.Forms.Button()
+        Me.dgvDados = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "CNPJ:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nome:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 137)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Telefone:"
+        '
+        'txtCNPJ
+        '
+        Me.txtCNPJ.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtCNPJ.Location = New System.Drawing.Point(106, 25)
+        Me.txtCNPJ.Name = "txtCNPJ"
+        Me.txtCNPJ.Size = New System.Drawing.Size(219, 20)
+        Me.txtCNPJ.TabIndex = 3
+        '
+        'txtNome
+        '
+        Me.txtNome.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtNome.Location = New System.Drawing.Point(106, 73)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(303, 20)
+        Me.txtNome.TabIndex = 4
+        '
+        'txtTelefone
+        '
+        Me.txtTelefone.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.txtTelefone.Location = New System.Drawing.Point(106, 134)
+        Me.txtTelefone.Name = "txtTelefone"
+        Me.txtTelefone.Size = New System.Drawing.Size(117, 20)
+        Me.txtTelefone.TabIndex = 5
+        '
+        'btnNovo
+        '
+        Me.btnNovo.BackColor = System.Drawing.Color.Lime
+        Me.btnNovo.Location = New System.Drawing.Point(24, 252)
+        Me.btnNovo.Name = "btnNovo"
+        Me.btnNovo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNovo.TabIndex = 6
+        Me.btnNovo.Text = "Novo"
+        Me.btnNovo.UseVisualStyleBackColor = False
+        '
+        'btnEditar
+        '
+        Me.btnEditar.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnEditar.Location = New System.Drawing.Point(174, 252)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEditar.TabIndex = 7
+        Me.btnEditar.Text = "Editar"
+        Me.btnEditar.UseVisualStyleBackColor = False
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.BackColor = System.Drawing.Color.Red
+        Me.btnExcluir.Location = New System.Drawing.Point(514, 252)
+        Me.btnExcluir.Name = "btnExcluir"
+        Me.btnExcluir.Size = New System.Drawing.Size(75, 23)
+        Me.btnExcluir.TabIndex = 8
+        Me.btnExcluir.Text = "Excluir"
+        Me.btnExcluir.UseVisualStyleBackColor = False
+        '
+        'btnExibir
+        '
+        Me.btnExibir.BackColor = System.Drawing.Color.Yellow
+        Me.btnExibir.Location = New System.Drawing.Point(334, 252)
+        Me.btnExibir.Name = "btnExibir"
+        Me.btnExibir.Size = New System.Drawing.Size(75, 23)
+        Me.btnExibir.TabIndex = 9
+        Me.btnExibir.Text = "Exibir"
+        Me.btnExibir.UseVisualStyleBackColor = False
+        '
+        'dgvDados
+        '
+        Me.dgvDados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvDados.BackgroundColor = System.Drawing.SystemColors.MenuBar
+        Me.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDados.Location = New System.Drawing.Point(12, 330)
+        Me.dgvDados.Name = "dgvDados"
+        Me.dgvDados.Size = New System.Drawing.Size(776, 295)
+        Me.dgvDados.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 637)
+        Me.Controls.Add(Me.dgvDados)
+        Me.Controls.Add(Me.btnExibir)
+        Me.Controls.Add(Me.btnExcluir)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNovo)
+        Me.Controls.Add(Me.txtTelefone)
+        Me.Controls.Add(Me.txtNome)
+        Me.Controls.Add(Me.txtCNPJ)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CRUD"
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtCNPJ As TextBox
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents txtTelefone As TextBox
+    Friend WithEvents btnNovo As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnExcluir As Button
+    Friend WithEvents btnExibir As Button
+    Friend WithEvents dgvDados As DataGridView
+End Class
---
+++ CRUD2/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private conexao As SqlConnection
+    Private comando As SqlCommand
+    Private da As SqlDataAdapter
+
+    Private strSQL As String
+
+    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+
+        Try
+            conexao = New SqlConnection("Server=DESKTOP-246GAH0\SQLEXPRESS; Database=CRUD;Trusted_Connection=True;")
+
+            strSQL = "INSERT INTO CAD_CRUD (CNPJ, NOME, TELEFONE) VALUES (@CNPJ, @NOME, @TELEFONE)"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@CNPJ", txtCNPJ.Text)
+            comando.Parameters.AddWithValue("@NOME", txtNome.Text)
+            comando.Parameters.AddWithValue("@TELEFONE", txtTelefone.Text)
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+        End Try
+    End Sub
+
+    Private Sub btnExibir_Click(sender As Object, e As EventArgs) Handles btnExibir.Click
+        Try
+            conexao = New SqlConnection("Server=DESKTOP-246GAH0\SQLEXPRESS; Database=CRUD;Trusted_Connection=True;")
+
+            strSQL = "SELECT * FROM CAD_CRUD"
+
+            comando = New SqlCommand(strSQL, conexao)
+            da = New SqlDataAdapter(strSQL, conexao)
+            Dim ds As New DataSet
+            da.Fill(ds)
+
+            dgvDados.DataSource = ds.Tables(0)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+        End Try
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        Try
+            conexao = New SqlConnection("Server=DESKTOP-246GAH0\SQLEXPRESS; Database=CRUD;Trusted_Connection=True;")
+
+            strSQL = "UPDATE CAD_CRUD SET CNPJ = @CNPJ, NOME = @NOME, TELEFONE = @TELEFONE WHERE CNPJ =@CNPJ"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@CNPJ", txtCNPJ.Text)
+            comando.Parameters.AddWithValue("@NOME", txtNome.Text)
+            comando.Parameters.AddWithValue("@TELEFONE", txtTelefone.Text)
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+        End Try
+    End Sub
+
+    Private Sub btnExcluir_Click(sender As Object, e As EventArgs) Handles btnExcluir.Click
+        Try
+            conexao = New SqlConnection("Server=DESKTOP-246GAH0\SQLEXPRESS; Database=CRUD;Trusted_Connection=True;")
+
+            strSQL = "DELETE CAD_CRUD WHERE CNPJ =@CNPJ"
+
+            comando = New SqlCommand(strSQL, conexao)
+            comando.Parameters.AddWithValue("@CNPJ", txtCNPJ.Text)
+            conexao.Open()
+            comando.ExecuteNonQuery()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            conexao.Close()
+            comando.Clone()
+            comando = Nothing
+            conexao = Nothing
+        End Try
+    End Sub
+End Class
